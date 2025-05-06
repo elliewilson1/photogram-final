c42f35bf8e39a94419fbb4ec0483fe09293128ba
elliewilson1 <ewilson0@chicagobooth.edu>
2025-05-06 18:32:57 +0000

    added form fields to sign up and edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
-  before_action(
-    :authenticate_user!, { :unless => :devise_controller? }
-  )
+
+  before_action(:authenticate_user!, { :unless => :devise_controller? })
+  before_action(:configure_permitted_parameters, { :if => :devise_controller? })
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      **{ :keys => [:username, :private] }
+    )
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      **{ :keys => [:username, :private] }
+    )
+  end
 end
