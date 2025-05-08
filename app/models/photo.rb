dc82f8384dd230e8eac57b6565b437e0038a27d5
elliewilson1 <ewilson0@chicagobooth.edu>
2025-05-08 01:36:54 +0000

    added image upload stuff

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,8 +12,10 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+  mount_uploader(:image, ImageUploader)
+
   # Direct associations
   belongs_to :user, required: true, class_name: "User", foreign_key: "owner_id"
-  has_many  :likes, class_name: "Like", foreign_key: "photo_id", dependent: :destroy
+  has_many  :likes,    class_name: "Like",    foreign_key: "photo_id", dependent: :destroy
   has_many  :comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,6 +42,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
-  #   "something.jpg"
+  #   "something.jpg" if original_filename
   # end
 end
